fdc19b4c413a0320990bd092a5ceff3964fee5f5
Dylane-Agoka <dylaneagoka47@gmail.com>
2025-10-18 10:57:35 +0100

    adding dynamic search

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,30 +1,75 @@
 const serachInput = document.querySelector('#search-input');
 
+const sectionDetails = document.querySelector('.section-details');
+
 serachInput.addEventListener('keydown', (event) => {
   if (event.key === 'Enter') {
+    sectionDetails.innerHTML = '';
     fectData();
   }
 });
 
 function fectData() {
   if (serachInput.value === null || serachInput.value === '') {
-    console.log('Please enter an input');
+    sectionDetails.innerHTML = '';
+    const emptySearch = document.createElement('div');
+    emptySearch.className = 'place-details';
+
+    const h3 = document.createElement('h3');
+    h3.textContent = 'Please enter an input';
+
+    emptySearch.appendChild(h3);
+    sectionDetails.appendChild(emptySearch);
   } else {
     fetch('travel_recommendation_api.json')
       .then(response => response.json())
       .then(data => {
-        switch(serachInput.value.toLowerCase().substring(0,5)) {
+        let datas = null;
+        switch (serachInput.value.toLowerCase().substring(0, 5)) {
           case 'beach':
-            console.log(data.beaches);
+            datas = data.beaches;
             break;
           case 'count':
-            console.log(data.countries);
+            datas = data.countries.map(d => d.cities).flat();
             break;
           case 'templ':
-            console.log(data.temples);
+            datas = data.temples;
             break;
           default:
-            console.log('No data found!');
+            datas = 'No data found!';
+        }
+        sectionDetails.innerHTML = '';
+        if (datas === 'No data found!') {
+          sectionDetails.innerHTML = '';
+          const noDatas = document.createElement('div');
+          noDatas.className = 'place-details';
+
+          const h3 = document.createElement('h3');
+          h3.textContent = 'No data found!';
+
+          noDatas.appendChild(h3);
+          sectionDetails.appendChild(noDatas);
+        } else {
+          datas.forEach(data => {
+            const placeDetails = document.createElement('div');
+            placeDetails.className = 'place-details';
+
+            const img = document.createElement('img');
+            img.src = `./${data.imageUrl}`;
+            img.alt = data.name;
+
+            const h3 = document.createElement('h3');
+            h3.textContent = data.name;
+
+            const p = document.createElement('p');
+            p.textContent = data.description;
+
+            placeDetails.appendChild(img);
+            placeDetails.appendChild(h3);
+            placeDetails.appendChild(p);
+
+            sectionDetails.appendChild(placeDetails);
+          });
         }
       })
       .catch(error => {
@@ -35,4 +80,5 @@ function fectData() {
 
 function clearData() {
   serachInput.value = '';
+  sectionDetails.innerHTML = '';
 }
